4a3ac1b3b337e1f39a7369c2804ddc42b9018405
m4klo <ul0258026@edu.uni.lodz.pl>
2023-05-08 20:20:04 +0200

    Tymczasowa zmiana w login na connect //problem pdo

--- login.php
+++ login.php
@@ -1,40 +1,31 @@
 <?php
 // Połączenie z bazą danych
-require_once 'connect_pdo.php';
+require_once 'connect.php';
 
 // Pobranie danych z formularza
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 // Znalezienie użytkownika o podanej nazwie
-$sql = "SELECT * FROM curator WHERE login=:username";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([':username' => $username]);
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-echo 'Password: ' . $password . '<br>';
-echo 'Hashed password from database: ' . $user['password'] . '<br>';
-
-var_dump($password);
-var_dump($user['password']);
-
+$sql = "SELECT * FROM curator WHERE login='$username'";
+$result = mysqli_query($conn, $sql);
+$user = mysqli_fetch_assoc($result);
 
 if ($user) {
     // Sprawdzenie, czy hasło jest poprawne
     if (password_verify($password, $user['password'])) {
-      // Hasło jest poprawne - zaloguj użytkownika
-      $_SESSION['user_id'] = $user['id'];
-      $_SESSION['username'] = $user['login'];
-      $_SESSION['location_id'] = $user['id_location'];
-      header('Location: generate_gallery.php');
-      error_log('Niepoprawne hasło dla użytkownika ' . $username);
-      exit;
+        // Hasło jest poprawne - zaloguj użytkownika
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['login'];
+        $_SESSION['location_id'] = $user['id_location'];
+        echo 'success';
+        exit;
     } else {
-      // Hasło jest niepoprawne
-      echo 'error';
+        // Hasło jest niepoprawne
+        echo 'error';
     }
-  } else {
+} else {
     // Użytkownik o podanej nazwie nie został znaleziony
     echo 'error';
-  }  
+}  
 ?>
